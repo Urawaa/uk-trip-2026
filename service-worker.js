a5256051317bc531dfd744fc199ec722d9b25ca4
Urawaa <urawaao@gmail.com>
2026-08-27 18:41:14 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>e.waitUntil(
-  self.registration.unregister().then(()=>self.clients.matchAll()).then(cs=>cs.forEach(c=>c.navigate(c.url)))
-));
